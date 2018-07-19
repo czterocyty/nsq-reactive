285a75e641292a4e31e905f048cdafbc3929beb8
adomanski <adomanski@gmail.com>
2018-07-19 20:06:15 +0200

    better Flux test for consumer, plus unifying a bit logging framework

--- src/test/java/org/czterocyty/nsq/NsqReceiverTest.java
+++ src/test/java/org/czterocyty/nsq/NsqReceiverTest.java
@@ -4,6 +4,9 @@ import com.github.brainlag.nsq.NSQMessage;
 import com.github.brainlag.nsq.NSQProducer;
 import com.github.brainlag.nsq.Nsq;
 import com.github.brainlag.nsq.exceptions.NSQException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
@@ -16,6 +19,7 @@ public class NsqReceiverTest {
     private static final String TOPIC_NAME = "receiver";
 
     private static final long NSQ_MSG_TIMEOUT = 5000;
+    private static final Logger LOGGER = LogManager.getLogger(NsqReceiverTest.class);
 
     @Test
     public void receiveSingleMessage() throws Exception {
@@ -29,8 +33,13 @@ public class NsqReceiverTest {
 
         StepVerifier.create(messages)
                 .thenRequest(1)
-                .expectNextMatches(msg -> "test-one-message".equals(new String(msg.getMessage())))
-                .consumeNextWith(NSQMessage::finished)
+                .consumeNextWith(msg -> {
+                    try {
+                        Assert.assertEquals("test-one-message", new String(msg.getMessage()));
+                    } finally {
+                        msg.finished();
+                    }
+                })
                 .thenCancel()
                 .verify(Duration.ofSeconds(5));
     }
@@ -43,21 +52,30 @@ public class NsqReceiverTest {
                 .channel("channel")
                 .stopOnConnectionError());
 
-        Flux<NSQMessage> messageProcessedFiveTimes = Flux.interval(
-                Duration.ofMillis(NSQ_MSG_TIMEOUT / 5 + 500))
+        Flux<NSQMessage> messageProcessedFiveTimes = Flux.interval(Duration.ofMillis(NSQ_MSG_TIMEOUT / 5 + 500))
+            .withLatestFrom(messages, (left, right) -> {
+                LOGGER.info("attempt {}", right.getAttempts());
+                return right;
+            })
+            .doOnNext(message -> {
+                LOGGER.info("Touching message");
+                message.touch();
+            })
             .take(5)
-            .zipWith(messages, (left, right) -> right)
-            .doOnNext(NSQMessage::touch);
+            .skip(4)
+            .log();
 
         produceMessage();
 
         StepVerifier.create(messageProcessedFiveTimes)
-                .thenRequest(5)
-                .expectNextMatches(msg -> {
-                    return "test-one-message".equals(new String(msg.getMessage()))
-                        && msg.getAttempts() == 1;
+                .consumeNextWith(record -> {
+                    try {
+                        Assert.assertEquals("test-one-message", new String(record.getMessage()));
+                        Assert.assertEquals(1, record.getAttempts());
+                    } finally {
+                        record.finished();
+                    }
                 })
-                .consumeNextWith(NSQMessage::finished)
                 .thenCancel()
                 .verify(Duration.ofSeconds(10));
     }
